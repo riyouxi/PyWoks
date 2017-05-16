ca115da07591898394fd9570777eb16a8986720b
riyouxi <1361508293@qq.com>
2017-05-16 22:56:00 +0800

    提交糗事百科爬虫demo

--- com/test/py1/pattern.py
+++ com/test/py1/pattern.py
@@ -20,20 +20,47 @@ class QSBK:
             print('页面加载失败')
             return None
         #<div.*?content">(.*?)</div>(.*?)<div class="stats.*?class="number">(.*?)</i>
-        pattern = re.compile('<h2>.*?(.*?)</h2>(.*?)<div.*?content">(.*?)</div>(.*?)<div class="stats.*?class="number">(.*?)</i>',re.S)
+        pattern = re.compile('<div class="author clearfix">.*?href.*?<img src.*?title=.*?<h2>(.*?)</h2>.*?<div.*?content">(.*?)</div>(.*?)<div class="stats.*?class="number">(.*?)</i>',re.S)
         items = re.findall(pattern,pageCode)
         pageStroies = []
         for item in items:
-            #print(item)
-            havImg = re.search("img", item[2])
-            if not havImg:
-                repalceBR = re.compile('<br/>')
-                text = re.sub(repalceBR, "\n", item[1])
-                pageStroies.append([item[0].strip(), item[1].strip(), item[2].strip(), item[3].strip()])
-                print(pageStroies)
-        #return  pageStroies
+            replaceBR = re.compile('<.?span>')
+            text = re.sub(replaceBR,"\n",item[1])
+            pageStroies.append([item[0].strip(), text.strip(), item[2].strip()])
 
+            print(pageStroies)
+        return  pageStroies
+
+    def loadPage(self):
+        if self.enable == True:
+            if len(self.stories)<2:
+                pageStroies = self.getPageItem(self.pageIndex)
+                if pageStroies:
+                    self.stories.append(pageStroies)
+                    self.pageIndex+=1
+
+    def getOneStroy(self,pageStories,page):
+        for story in pageStories:
+            inputStr = input()
+            self.loadPage()
+            if inputStr =='Q':
+                self.enable = False
+                return
+            print("第%d页\t发布人:%s\t内容：%s"%(page,story[0],story[1]))
+
+    def start(self):
+        print('正在读取糗事而百科段子。。。')
+        self.enable =True
+        self.loadPage()
+        nowPage = 0
+        while self.enable:
+            if len(self.stories)>0:
+                pageStroies = self.stories[0]
+                nowPage+=1
+                del self.stories[0]
+                self.getOneStroy(pageStroies,nowPage)
 
 
 qs = QSBK()
-qs.getPageItem(1)
+qs.start()
+
